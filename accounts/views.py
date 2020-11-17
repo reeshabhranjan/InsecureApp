e079957a8f923fd3eb83843de5d0b0390468cc51
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-11-17 18:52:39 +0530

    Add login, logout, signup and dashboard.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
+
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful, proceed to login.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', context={'form': form})
---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from dashboard import views
+
+urlpatterns = [
+    path('home/', views.home, name='home')
+]
