73b9b19a21eb7cf13a2cd4988bd40f0605bb4c3e
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-11-17 19:26:38 +0530

    Allow XSS attack.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    body = models.TextField(null=False, blank=False)
+    time_added = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from dashboard.models import Post
 
 
 @login_required
 def home(request):
-    return render(request, 'dashboard/home.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'dashboard/home.html', context=context)
