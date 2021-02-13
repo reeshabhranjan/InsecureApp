b5578a4fc0d7a26395b3e313b81834e09927f021
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-02-13 17:11:24 +0530

    Take SECRET_KEY from environment variables, allow heroku site to host the website.

--- InsecureApp/settings.py
+++ InsecureApp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ag!l%i#haftzs22fru80f(x&53@j&cjgvlb*oc8vdea7l+r3hq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d4rkc0de-websession-01.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
